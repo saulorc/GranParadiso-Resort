8b3571d33592c537b3e135145450cd1e55484882
saulorc <33528732+saulorc@users.noreply.github.com>
2025-09-29 13:39:36 -0300

    Atualizar o vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/GranParadiso-Resort/",
+
+// ajuste o base para o nome do repositório GitHub Pages
+export default defineConfig({
+  base: '/GranParadiso-Resort/',
+  plugins: [react()],
   server: {
     host: "::",
     port: 8080,
-  },
+})
+
+
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
